45b77eda888e3903353c8c855383c5e68cdbe9e4
riodwimulyono0512-sudo <riodwimulyomo0512@gmail.com>
2026-05-14 21:12:29 +0700

    18

--- tugasss.cpp
+++ tugasss.cpp
@@ -114,6 +114,9 @@ int main()
         case 4:
             cout << "Exiting program." << endl;
             break;
+        default:
+            cout << "invalid choice. Try again." << endl;
+            break;
 
 
 
