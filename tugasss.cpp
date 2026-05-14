04440dbd43b60f386cc79683c1d768cb62353c32
riodwimulyono0512-sudo <riodwimulyomo0512@gmail.com>
2026-05-14 21:05:55 +0700

    7

--- tugasss.cpp
+++ tugasss.cpp
@@ -44,3 +44,26 @@ public:
         top = top->next;
         cout << "Popped value: " << temp->data << endl;
     }
+
+     void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+    bool isEmpty()
+    {
+        return top == NULL;
+    }
+};
